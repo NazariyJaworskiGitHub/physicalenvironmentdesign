443f461ad83b6606a238923bb5eff2f252df3176
Passazhur@gmail.com <Passazhur@gmail.com@fbf05d7f-0995-0f14-e1a3-7aaf52e8406a>
2014-03-04 16:25:20 +0000

    Added new (old) Node class (and subclasses), refused previously used Qt-Vector classes; Added PLC render for segments, with native functions to JavaScript-WebGL buffers loading;

--- FEM/node.h
+++ FEM/node.h
@@ -1,96 +1,272 @@
+/// \author Nazariy Jaworski (c)
+/// \date 01.08.2013
+
 #ifndef NODE_H
 #define NODE_H
 
-#include <QVector4D>
+#include <cmath>
+#include <stdarg.h>
+#include <memory.h>
+#include <stdexcept>
+
+#include <qglobal.h>
+
 #include <QVector3D>
-#include <QVector2D>
 
 namespace FEM
 {
-    /// implements [] operator for QVector4D (need for FiniElement class)
-    class Node4D : public QVector4D
-    {
-        public: Node4D(){}
-        public: Node4D(const qreal xp, const qreal yp, const qreal zp, const qreal wp): QVector4D(xp,yp,zp,wp){}
-        public: Node4D(const Node4D &target): QVector4D(target.x(),target.y(),target.z(),target.w()){}
-        public: qreal operator [](const int index) throw(std::out_of_range)
-        {
-            switch (index)
-            {
-            case 0:
-                return x();
-            case 1:
-                return y();
-            case 2:
-                return z();
-            case 3:
-                return w();
-            default:
-                throw std::out_of_range("Node4D[i], i out of range");
-            }
-        }
-    };
+    template <int _dim_, typename _DimType_ = double> class Node;
+    template <int _dim_, typename _DimType_ = double> using Vector = Node<_dim_, _DimType_>;
 
-    /// implements [] operator for QVector3D (need for FiniElement class)
-    class Node3D : public QVector3D
-    {
-        public: Node3D(){}
-        public: Node3D(const qreal xp, const qreal yp, const qreal zp): QVector3D(xp,yp,zp){}
-        public: Node3D(const Node3D &target): QVector3D(target.x(),target.y(),target.z()){}
-        public: qreal operator [](const int index) throw(std::out_of_range)
-        {
-            switch (index)
-            {
-            case 0:
-                return x();
-            case 1:
-                return y();
-            case 2:
-                return z();
-            default:
-                throw std::out_of_range("Node3D[i], i out of range");
-            }
-        }
-    };
+    typedef Node<1> Node1D;
+    typedef Vector<1> Vector1D;
+    typedef Node<2> Node2D ;
+    typedef Vector<2> Vector2D;
+    typedef Node<3> Node3D ;
+    typedef Vector<3> Vector3D;
+    typedef Node<4> Node4D ;
+    typedef Vector<4> Vector4D;
 
-    /// implements [] operator for QVector2D (need for FiniElement class)
-    class Node2D : public QVector2D
-    {
-        public: Node2D(){}
-        public: Node2D(const qreal xp, const qreal yp): QVector2D(xp,yp){}
-        public: Node2D(const Node2D &target): QVector2D(target.x(),target.y()){}
-        public: qreal operator [](const int index) throw(std::out_of_range)
-        {
-            switch (index)
-            {
-            case 0:
-                return x();
-            case 1:
-                return y();
-            default:
-                throw std::out_of_range("Node2D[i], i out of range");
-            }
-        }
-    };
 
-    /// implements [] operator for qreal (need for FiniElement class)
-    class Node1D
+    /// \brief The Node class provides simple support of Nodes and Vectors \n
+    /// Node \f$ u\in DimType^n \f$, where \c dim is the size of coordinates array, i.e. \f$ n \f$. \n
+    /// Another name for this class is Vector.
+    template <int _dim_, typename _DimType_> class Node
     {
-        private: qreal _xPos;
-        public: Node1D(){}
-        public: Node1D(const qreal xp):_xPos(xp){}
-        public: Node1D(const Node1D &target): _xPos(target._xPos){}
-        public: qreal operator [](const int index) throw(std::out_of_range)
+    public:
+        enum COORDINATES {X=0, Y, Z, W};
+
+        _DimType_ coord[_dim_];  ///< Coordinates array.
+
+        /// \brief Common constructor
+        Node()
         {
-            switch (index)
+            memset(coord,0,_dim_*sizeof(_DimType_));
+        }
+
+        /// \brief Constructor. \n
+        /// \warning It takes only \c dim arguments from argument list. \n
+        /// Also make shure that all parameters are \c _DimType_.
+        Node( const _DimType_ x, ...)
+        {
+            coord[0] = x;
+            va_list _coordinates;
+            va_start(_coordinates, x);
+            for(int i=1;i<_dim_;++i)
+                coord[i]=va_arg (_coordinates, _DimType_);
+            va_end(_coordinates);
+        }
+
+        /// \brief Copy constructor. \n
+        /// \warning Bouth nodes shoulde be at the same dimension.
+        Node( const Node &n )
+        {
+            for(int i=0;i<_dim_;++i)
+                coord[i]=n.coord[i];
+        }
+
+        Node &operator =(const Node &target)
+        {
+            for(int i=0;i<_dim_;++i)
+                coord[i]=target.coord[i];
+            return *this;
+        }
+
+        /// \brief Square distance to target Node
+        /// \li \f$ \parallel u-v \parallel^2 = \sum_{i=1}^n(u_i-v_i)^2,u\in\Re^n,v\in\Re^n \f$.
+        /// \li See <a href="http://en.wikipedia.org/wiki/Euclidean_distance">Euclidean distance</a>.
+        /// \param[in] target Node, distance to which should be calculated, i.e. \f$ v \f$.
+        /// \return Square distance.
+        /// \warning Bouth nodes shoulde be at the same dimension.
+        _DimType_ distanceSquare( const Node &target )
+        {
+            /// \todo bad constatnt
+            _DimType_ _dist = 0.0;
+            for(int i=0;i<_dim_;++i)
+                _dist+=(coord[i]-target.coord[i])*(coord[i]-target.coord[i]);
+            return _dist;
+        }
+
+        /// \brief Makes the Vector normalized
+        /// \li \f$ \widehat{u}=\frac{u}{\parallel u\parallel}, u\in\Re^n,\parallel u\parallel\neq0 \f$.
+        /// \li See <a href="http://en.wikipedia.org/wiki/Normalized_vector">Unit vector</a>.
+        void normalize()
+        {
+            /// \todo bad constatnt
+            _DimType_ _length = 0.0;
+            for(int i=0;i<_dim_;++i)
+                _length+=coord[i]*coord[i];
+            if(_length>0.0)
             {
-            case 0:
-                return _xPos;
-            default:
-                throw std::out_of_range("Node1D[i], i out of range");
+                _length = sqrt(_length);
+                for(int i=0;i<_dim_;++i)
+                    coord[i]/=_length;
             }
         }
+
+        /// \brief Calculates the dot product with target Vector
+        /// \li \f$ \langle u,v\rangle=\sum_{i=1}^nu_iv_i,u\in\Re^n,v\in\Re^n \f$.
+        /// \li See <a href="http://en.wikipedia.org/wiki/Dot_product">Dot product</a>.
+        /// \param[in] target Vector, dot product with which should be calculated, i.e. \f$ v \f$.
+        /// \return Dot product.
+        /// \warning Bouth nodes shoulde be at the same dimension.
+        _DimType_ dotProduct( const Node &target )
+        {
+            /// \todo bad constatnt
+            _DimType_ _product = 0.0;
+            for(int i=0;i<_dim_;++i)
+                _product+=coord[i]*target.coord[i];
+            return _product;
+        }
+
+        /// \brief Multiplies Vector on scalar.
+        /// \param[in] scalar .
+        void multiply( _DimType_ scalar)
+        {
+            for(int i=0;i<_dim_;++i)
+                coord[i]*=scalar;
+        }
+
+        Node &operator *= (_DimType_ scalar)
+        {
+            for(int i=0;i<_dim_;++i)
+                coord[i]*=scalar;
+            return *this;
+        }
+
+        /// \brief Divides Vector on scalar.
+        /// \param[in] scalar .
+        void divide(_DimType_ scalar)
+        {
+            for(int i=0;i<_dim_;++i)
+                coord[i]/=scalar;
+        }
+        Node &operator /= (_DimType_ scalar)
+        {
+            for(int i=0;i<_dim_;++i)
+                coord[i]/=scalar;
+            return *this;
+        }
+
+        /// \brief Adds scalar to Vector.
+        /// \param[in] scalar .
+        void addScalar(_DimType_ scalar)
+        {
+            for(int i=0;i<_dim_;++i)
+                coord[i]+=scalar;
+        }
+
+        Node &operator += (_DimType_ scalar)
+        {
+            for(int i=0;i<_dim_;++i)
+                coord[i]+=scalar;
+            return *this;
+        }
+
+        /// \brief Adds target Vector to Vector.
+        /// \param[in] target .
+        /// \warning Bouth vectors shoulde be at the same dimension.
+        void addVector(const Node &target)
+        {
+            for(int i=0;i<_dim_;++i)
+                coord[i]+=target.coord[i];
+        }
+
+        Node &operator += (const Node &target)
+        {
+            for(int i=0;i<_dim_;++i)
+                coord[i]+=target.coord[i];
+            return *this;
+        }
+
+        /// \brief Subtracts target Vector from Vector.
+        /// \param[in] target .
+        /// \warning Bouth vectors shoulde be at the same dimension.
+        void subtractVector(const Node &target)
+        {
+            for(int i=0;i<_dim_;++i)
+                coord[i]-=target.coord[i];
+        }
+
+        Node &operator -= (const Node &target)
+        {
+            for(int i=0;i<_dim_;++i)
+                coord[i]-=target.coord[i];
+            return *this;
+        }
+
+        /// \brief Check if the target Node is equal to Node.
+        /// \param target .
+        /// \param eps Checking precision, default value is \c 1e-8
+        /// \return \c true if equal.
+        /// \warning Bouth nodes shoulde be at the same dimension.
+        bool equal(const Node &target, _DimType_ const &eps = 1e-8)
+        {
+            for(int i=0;i<_dim_;++i)
+                if(fabs(coord[i]-target.coord[i])>eps)
+                    return false;
+            return true;
+        }
+
+        /// \brief operator == \n See Node::equal(const Node &target, _DimType_ const &eps = 1e-8).
+        /// \param target .
+        /// \return \c true if equal.
+        bool operator == (const Node &target)
+        {
+            return equal(target);
+        }
+
+        bool operator != (const Node &target)
+        {
+            return !equal(target);
+        }
+
+        /// \brief Calculates the cross product with target Vector3D,
+        /// \warning it works only in \f$ \Re^3 \f$.
+        /// \li \f$ \left[u,v\right]=u\times v=\begin{bmatrix}i & j & k \\u_x & u_y & u_z
+        /// \\v_x & v_y & v_z \end{bmatrix}=\begin{bmatrix}u_y & u_z \\v_y & v_z
+        /// \end{bmatrix}i-\begin{bmatrix}u_x & u_z \\v_x & v_z \end{bmatrix}j+
+        /// \begin{bmatrix}u_x & u_y \\v_x & v_y \end{bmatrix}k \f$.
+        /// \li See <a href="http://en.wikipedia.org/wiki/Cross_product">Cross product</a>.
+        /// \param[in] target Vector3D, cross product with which should be calculated, i.e. \f$ v \f$.
+        /// \return Pointer to new Vector3D, which equal to cross product.
+        Vector3D *crossProduct( const Vector3D &target )
+        {
+            Vector3D *_cross = new Vector3D();
+            _cross->coord[0] = this->coord[1]*target.coord[2] - this->coord[2]*target.coord[1];
+            _cross->coord[1] = this->coord[2]*target.coord[0] - this->coord[0]*target.coord[2];
+            _cross->coord[2] = this->coord[0]*target.coord[1] - this->coord[1]*target.coord[0];
+            return _cross;
+        }
+
+        _DimType_ & operator [](int index) throw(std::out_of_range)
+        {
+            if(index<0 || index>=_dim_)
+                throw std::out_of_range("Node[i], i out of range");
+            else return coord[index];
+        }
+
+        bool isNull() const
+        {
+            for(int i=0; i<_dim_; ++i)
+                /// \todo bad constant
+                if(!qIsNull(coord[i])) return false;
+            return true;
+        }
+
+        Node &operator () ( const _DimType_ x, ...)
+        {
+            coord[0] = x;
+            va_list _coordinates;
+            va_start(_coordinates, x);
+            for(int i=1;i<_dim_;++i)
+                coord[i]=va_arg (_coordinates, _DimType_);
+            va_end(_coordinates);
+            return *this;
+        }
+
+        /// \brief Common destructor
+        ~Node() {}
     };
 }
-
 #endif // NODE_H
