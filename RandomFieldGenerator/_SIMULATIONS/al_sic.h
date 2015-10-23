18355f2959c0a6e240c96f36458f31e8d5eec7ee
passazhur <passazhur@gmail.com>
2015-10-23 14:03:03 +0300

    Hardcoded Tavangar Test

--- RandomFieldGenerator/_SIMULATIONS/al_sic.h
+++ RandomFieldGenerator/_SIMULATIONS/al_sic.h
@@ -37,7 +37,7 @@ namespace Simulation
 
         FEM::Domain RVEDomain(RVE);
         RVEDomain.addMaterial(0,0.5,Matrix);
-        RVEDomain.addMaterial(0.5,2,Phase);   // 1<2 (excluding max intensity
+        RVEDomain.addMaterial(0.5,2,Phase);   // 1<2 (excluding max intensity)
 
         std::ofstream OutputFile;
         OutputFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
@@ -113,6 +113,130 @@ namespace Simulation
         std::cout << "Total: " << timer.getTimeSpanAsString() << " seconds" << std::endl;
         OutputFile << "Total: " << timer.getTimeSpanAsString() << " seconds" << std::endl;
         OutputFile.close();
+    }
+
+    // 23.10.2015
+    inline void TavangarTest()
+    {
+        //                          h       unused  unused  unused  unused
+        FEM::Characteristics Ag{     370.0,      0,      0,      0,      0};
+//        FEM::Characteristics LD8110{1140.0,      0,      0,      0,      0};  // err = 0.6%
+//        FEM::Characteristics LD8120{1180.0,      0,      0,      0,      0};  // err = 0.7%
+//        FEM::Characteristics LD8160{1220.0,      0,      0,      0,      0};  // err = 0.9%
+        FEM::Characteristics LD8110{1200.0,      0,      0,      0,      0};
+        FEM::Characteristics LD8120{1200.0,      0,      0,      0,      0};
+        FEM::Characteristics LD8160{1200.0,      0,      0,      0,      0};
+
+        int RVEDiscreteSize = 128;
+        float RVEPhysicalLength = 0.005;
+        int R_LD8110 = (float)(RVEDiscreteSize) / RVEPhysicalLength * 0.00045 / 2.0;
+        int R_LD8120 = (float)(RVEDiscreteSize) / RVEPhysicalLength * 0.00045 / 2.0;
+        int R_LD8160 = (float)(RVEDiscreteSize) / RVEPhysicalLength * 0.00035 / 2.0;
+        int R_MDB4 = (float)(RVEDiscreteSize) / RVEPhysicalLength * 0.000052 / 2.0;
+
+        Timer timer;
+        timer.start();
+
+        std::ofstream OutputFile;
+        OutputFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        std::stringstream _filename;
+        _filename << "RVE" << RVEDiscreteSize << "Tavangar.txt";
+        OutputFile.open(_filename.str());
+        OutputFile << "RVE "<< RVEPhysicalLength << "m "
+                   << RVEDiscreteSize << "x" << RVEDiscreteSize << "x" << RVEDiscreteSize << "\n";
+        OutputFile << "Matrix Ag:\nh=" << Ag.heatConductionCoefficient << "\n";
+        OutputFile << "Phase LD8110:\nh=" << LD8110.heatConductionCoefficient << "\n";
+        OutputFile << "Phase LD8120:\nh=" << LD8120.heatConductionCoefficient << "\n";
+        OutputFile << "Phase LD8160:\nh=" << LD8160.heatConductionCoefficient << "\n";
+        OutputFile << "R_LD8110 = " << R_LD8110 << "\n";
+        OutputFile << "R_LD8120 = " << R_LD8120 << "\n";
+        OutputFile << "R_LD8160 = " << R_LD8160 << "\n";
+        OutputFile << "R_MDB4 = " << R_MDB4 << "\n";
+
+        float effh, minh, maxh;
+        int n;
+        float PhaseVol;
+        float targetVol;
+
+        // R_LD8110 61% ----------------------------------------------------------------
+        {
+            RepresentativeVolumeElement RVE(RVEDiscreteSize,RVEPhysicalLength);
+            FEM::Domain RVEDomain(RVE);
+            RVEDomain.addMaterial(0,0.5,Ag);
+            RVEDomain.addMaterial(0.5,2,LD8110);
+            n=0;
+            PhaseVol = 0;
+            targetVol = 0.61;
+            for(;;)
+            {
+                RVE.generateOverlappingRandomEllipsoidsIntenseCL(1,R_LD8110,R_LD8110,0);
+                ++n;
+                PhaseVol = RVEDomain.getMaterialVolumeConcentration(1.0f);
+                std::cout << n << " " << PhaseVol << " " << targetVol << "\n";
+                if(PhaseVol >= targetVol) break;
+            }
+            Synthesis::getEffectiveHeatConductionCharacteristic(
+                        RVEDomain, effh, minh, maxh, 1e-5);
+            std::cout << "[" << n << "] vol=" << PhaseVol
+                      << " effh=" << effh << " minh=" << minh << " maxh=" << maxh << "\n";
+            OutputFile << n << " " << PhaseVol << " "
+                       << effh << " " << minh << " " << maxh << "\n";
+            OutputFile.flush();
+        }
+        // R_LD8120 62% ----------------------------------------------------------------
+        {
+            RepresentativeVolumeElement RVE(RVEDiscreteSize,RVEPhysicalLength);
+            FEM::Domain RVEDomain(RVE);
+            RVEDomain.addMaterial(0,0.5,Ag);
+            RVEDomain.addMaterial(0.5,2,LD8120);
+            n=0;
+            PhaseVol = 0;
+            targetVol = 0.62;
+            for(;;)
+            {
+                RVE.generateOverlappingRandomEllipsoidsIntenseCL(1,R_LD8120,R_LD8120,0);
+                ++n;
+                PhaseVol = RVEDomain.getMaterialVolumeConcentration(1.0f);
+                std::cout << n << " " << PhaseVol << " " << targetVol << "\n";
+                if(PhaseVol >= targetVol) break;
+            }
+            Synthesis::getEffectiveHeatConductionCharacteristic(
+                        RVEDomain, effh, minh, maxh, 1e-5);
+            std::cout << "[" << n << "] vol=" << PhaseVol
+                      << " effh=" << effh << " minh=" << minh << " maxh=" << maxh << "\n";
+            OutputFile << n << " " << PhaseVol << " "
+                       << effh << " " << minh << " " << maxh << "\n";
+            OutputFile.flush();
+        }
+        // R_LD8160 65% ----------------------------------------------------------------
+        {
+            RepresentativeVolumeElement RVE(RVEDiscreteSize,RVEPhysicalLength);
+            FEM::Domain RVEDomain(RVE);
+            RVEDomain.addMaterial(0,0.5,Ag);
+            RVEDomain.addMaterial(0.5,2,LD8160);
+            n=0;
+            PhaseVol = 0;
+            targetVol = 0.65;
+            for(;;)
+            {
+                RVE.generateOverlappingRandomEllipsoidsIntenseCL(1,R_LD8160,R_LD8160,0);
+                ++n;
+                PhaseVol = RVEDomain.getMaterialVolumeConcentration(1.0f);
+                std::cout << n << " " << PhaseVol << " " << targetVol << "\n";
+                if(PhaseVol >= targetVol) break;
+            }
+            Synthesis::getEffectiveHeatConductionCharacteristic(
+                        RVEDomain, effh, minh, maxh, 1e-5);
+            std::cout << "[" << n << "] vol=" << PhaseVol
+                      << " effh=" << effh << " minh=" << minh << " maxh=" << maxh << "\n";
+            OutputFile << n << " " << PhaseVol << " "
+                       << effh << " " << minh << " " << maxh << "\n";
+            OutputFile.flush();
+            timer.stop();
+            std::cout << "Total: " << timer.getTimeSpanAsString() << " seconds" << std::endl;
+            OutputFile << "Total: " << timer.getTimeSpanAsString() << " seconds" << std::endl;
+            OutputFile.close();
+        }
     }
 }
 
--- RandomFieldGenerator/main.cpp
+++ RandomFieldGenerator/main.cpp
@@ -50,11 +50,14 @@ int main(int argc, char *argv[])
 //    run_tests_all();
 //    _consoleRunner.writeToOutput("Tests done\n");
 
-    //                          h       E       v       LCTE    unused
-    FEM::Characteristics Al{     210.0,   68.0, 0.36,   25.50,  0};
-    FEM::Characteristics SiC{    125.6,  410.0, 0.14,    4.51,  0};
-    FEM::Characteristics C{     2000.0, 1220.0, 0.20,    1.10,  0};
-    Simulation::SphericalInclusionsSimulationTest(32, 0.01, Al, C, 50);
+//    //                          h       E       v       LCTE    unused
+//    FEM::Characteristics Al{     210.0,   68.0, 0.36,   25.50,  0};
+//    FEM::Characteristics SiC{    125.6,  410.0, 0.14,    4.51,  0};
+//    FEM::Characteristics C{     2000.0, 1220.0, 0.20,    1.10,  0};
+//    Simulation::SphericalInclusionsSimulationTest(32, 0.01, Al, C, 50);
+
+    // 23.10.2015
+    Simulation::TavangarTest();
 
     ///////////////////////////////////////////////////////////////////////////////////////
 //    Timer _TotalCalculationTimer;
